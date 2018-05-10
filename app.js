3d222ee8a1ac3fa06427a3a28bb0b5e370522b45
Rob Hixson <rrh62093@gmail.com>
2018-05-09 21:53:25 -0400

    Remove logs and clean up code

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ var xlsx = require('xlsx');
 //var workbook = xlsx.readFile('GrowthStocks.xlsx');
 var workbook = xlsx.readFile('test.xlsx');
 var workbookJson = xlsx.utils.sheet_to_json(workbook.Sheets['Export']);
-//console.log(workbookJson);
 var finalStockInfo = {};
 workbookJson.forEach(row => {
     var symbol = row.Symbol;
-    console.log(symbol);
     finalStockInfo[symbol] = {};
     finalStockInfo[symbol]['StrengthRating'] = row['RS Rating'];
     getPriceAndVolume(symbol, finalStockInfo);
@@ -33,27 +31,10 @@ setTimeout(function(){
                     }
                 }
             }
-            console.log(symbol, finalStockInfo[symbol]);
         }
     });
 
 }, 9000);
-
-
-//Taking it in as a prompt
-// prompt.start();
-// prompt.get(['ticker'], (err, result) => {
-//     var stocks = result.ticker.split(',');
-//     var finalStockInfo = {};
-//     stocks.forEach(stock => {
-//         finalStockInfo[stock] = {};
-//         getPriceAndVolume(stock, finalStockInfo);
-//         getPriceAndVolume(stock, finalStockInfo);
-//         getMovingAverage(stock, 'EMA', finalStockInfo);
-//         getMovingAverage(stock, 'SMA', finalStockInfo);
-//     });
-//     setTimeout(function(){console.log(finalStockInfo)}, 2000);
-// });
 
 function getPriceAndVolume(ticker, finalStockInfo){
     var url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&apikey=GYWUJPM4V0P0WGF8`;
@@ -80,7 +61,6 @@ function getPriceAndVolume(ticker, finalStockInfo){
     })
 }
 
-
 function getMovingAverage(ticker, type, timePeriod, finalStockInfo){
     var url = `https://www.alphavantage.co/query?function=${type}&symbol=${ticker}&interval=daily&time_period=${timePeriod}&series_type=close&apikey=GYWUJPM4V0P0WGF8`;
     request(url, {json: true}, (err, res, body) => {
@@ -102,22 +82,6 @@ function getMovingAverage(ticker, type, timePeriod, finalStockInfo){
     });
 }
 
-function getPercentDifference(price, average){
-    return Math.abs(((price - average) / average)*100);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
 function getPricesAndVolume(listOfStocks){
     var url = `https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=${listOfStocks}&apikey=GYWUJPM4V0P0WGF8`;
     request(url, {json: true}, (err, res, body) => {
@@ -125,4 +89,8 @@ function getPricesAndVolume(listOfStocks){
         console.log(res.body['Stock Quotes']);
         return res.body['Stock Quotes'];
     });
+}
+
+function getPercentDifference(price, average){
+    return Math.abs(((price - average) / average)*100);
 }
